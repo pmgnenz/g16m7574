8a6d1bc3f8ac927a7cf7551196e8c8ced471ec63
Gibson Muniitha <g16m7574@ict.ru.ac.za>
2019-02-21 15:36:43 +0200

    changes

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,37 @@
 
 namespace Problems
 {
-    class Program
+   public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             Console.WriteLine(Prefix("hello"));
+              Console.WriteLine(Prefix(""));
+            Console.WriteLine(Prefix("what    ...  did you say?? "));
+            Console.ReadLine();
+        }
+        static string Prefix(string inp)
+        {
+            int strlength = inp.Length;
+            int wordscount = 0;
+            string str;
+            string[] words = inp.Split(' ');
+            if (strlength != 0)
+            {
+                foreach (string word in words)
+                {
+                    if (word != "")
+                    {
+                        wordscount++;
+                    }
+                }
+            }
+            
+            str = strlength + "," + wordscount + ":" + inp;
+            return str;
+
+
+
         }
     }
 }
