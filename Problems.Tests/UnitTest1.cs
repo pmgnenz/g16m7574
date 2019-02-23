f4f9eb25b965cb341f9869b90c20c5d992667df7
Gibson <munenepmg@gmail.com>
2019-02-23 14:27:55 +0300

    single test method but multiple items of data

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -15,22 +15,19 @@ namespace Tests
         {
            Program p = new Program();
             string result = p.Prefix("hello");
+             string result2 = p.Prefix("");
+             string result3 = p.Prefix("what    ...  did you say?? ");
+             string result4 = p.Prefix("Gibson");
+             string result5 = p.Prefix("who are you??");
+             string result6 = p.Prefix("  fun ");
+
             Assert.AreEqual("5,1:hello",result);
+              Assert.AreEqual("0,0:",result2);
+              Assert.AreEqual("27,5:what    ...  did you say?? ",result3);
+               Assert.AreEqual("6,1:Gibson",result4);
+                Assert.AreEqual("13,3:who are you??",result5);
+                 Assert.AreEqual("6,1:  fun ",result6);
         }
 
-          [Test]
-         public void Test2()
-        {
-           Program p = new Program();
-            string result = p.Prefix("");
-            Assert.AreEqual("0,0:",result);
-        }
-        [Test]
-         public void Test3()
-        {
-           Program p = new Program();
-            string result = p.Prefix("what    ...  did you say?? ");
-            Assert.AreEqual("27,5:what    ...  did you say?? ",result);
-        }
     }
 }   
